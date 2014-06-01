4c12f0d749cb41b84ed44fe73d09642074706f9a
macrl <mac_rl@air-de-mac.home>
2014-06-01 22:46:48 +0200

    Test dans main

--- src/person/TestPerson.java
+++ src/person/TestPerson.java
@@ -8,8 +8,10 @@ public class TestPerson {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hello world !");
-        Person p=new Person("Mac",17);
-        p.affiche();
+        Person p1=new Person("Mac",17);
+        p1.affiche();
+        Person p2=new Person("Lee",18);
+        p2.affiche();
 	}
 
 }
