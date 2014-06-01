c58ec7a901ef6e90b3572b1408c3108d9b9fdec3
macrl <mac_rl@air-de-mac.home>
2014-06-01 22:37:44 +0200

    Definition de la classe Person

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,5 +1,32 @@
 package person;
 
 public class Person {
+	private String nom;
+	private int age;
+	public Person(String nom, int age) {
+		super();
+		this.nom = nom;
+		this.age = age;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "Person [nom=" + nom + ", age=" + age + "]";
+	}
+	public void affiche()
+	{
+		System.out.println(this.toString());
+	}
 
 }
--- src/person/TestPerson.java
+++ src/person/TestPerson.java
@@ -8,6 +8,8 @@ public class TestPerson {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hello world !");
+        Person p=new Person("Mac",17);
+        p.affiche();
 	}
 
 }
